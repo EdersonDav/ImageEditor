ad35fab13f8987d299c3f11f45dd275480dd78d4
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-06-17 20:20:38 -0300

    Editor image end

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
-const sharp = require('sharp');
+const sharp = require("sharp");
+const compress_images = require("compress-images");
+const fs = require("fs");
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
-function resize(path, width) {
-  sharp(path).resize({ width: width })
-    .toFile(`./temp/output_resize${width}.jpg`, (error) => {
+function resize(path, pathOutput, width) {
+  sharp(path)
+    .resize({ width: width })
+    .toFile(pathOutput, (error) => {
       if (error) {
         console.log(error);
       } else {
-        console.log("Imagem rediensionada com sucesso!")
+        console.log("Imagem rediensionada com sucesso!");
+        compress(pathOutput, "./compressed/");
       }
-    })
+    });
 }
-resize(path, width)
+
+function compress(INPUT_path, OUTPUT_path) {
+  compress_images(
+    INPUT_path,
+    OUTPUT_path,
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    {
+      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+    },
+    function (error, completed, statistic) {
+      console.log("-------------");
+      console.log(error);
+      console.log(completed);
+      console.log(statistic);
+      console.log("-------------");
+    }
+  );
+  fs.unlink(INPUT_path, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("sucess");
+    }
+  });
+}
+
+resize(path, `./temp/output_resize${width}.jpg`, width);
